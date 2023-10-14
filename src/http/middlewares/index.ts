b3910e1987d4e13dc237f4546083abd01f8a54a1
heavenmanspb <heavenmanspb@gmail.com>
2023-10-14 23:31:55 +0700

    fix

--- src/http/middlewares/index.ts
+++ src/http/middlewares/index.ts
@@ -3,8 +3,5 @@ import type { FastifyInstance } from 'fastify'
 import { xResponseTimeMiddle } from './xResponseTime.js'
 
 export default fp(async (fastify: FastifyInstance) => {
-    fastify.addHook('onSend', async (req, res, payload) => {
-        if (payload === undefined) res.code(204)
-    })
     fastify.addHook('onSend', xResponseTimeMiddle)
 })
